571d47ae82c12b9043c7fd1d4a9b83eb62ed8b0e
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-06-06 01:23:54 +0900

    view folder list routing

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index() {
-        return 'lorem';
+        $folders = Folder::all();   // 全てのフォルダデータ取得
+
+        return view('tasks/index', [
+            'folders'   =>  $folders,   // テンプレートにデータを受け渡し
+        ]);
     }
 }
