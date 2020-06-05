5a5bb4e266bdb018875c2812cb7ad954d25f8017
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-06-06 01:58:16 +0900

    task status

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id) {
 
-        $folders = Folder::all();   // 全てのフォルダデータ取得
+        // 全てのフォルダデータ取得
+        $folders = Folder::all();
+
+        // 選ばれたフォルダを取得 $idと一致するフォルダ
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得 whereはSQLのwhere区と同様
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
 
         return view('tasks/index', [
             'folders'   =>  $folders,   // テンプレートにデータを受け渡し
-            'current_folder_id' =>  $id,
+            'current_folder_id' =>  $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
